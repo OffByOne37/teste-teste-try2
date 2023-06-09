24c326d55f885524e8b6cd18e73f57013c139853
OffByOne37 <129181101+OffByOne37@users.noreply.github.com>
2023-06-09 13:44:06 +0200

    Aktualisiere custom.ts

--- custom.ts
+++ custom.ts
@@ -7,7 +7,7 @@ namespace music {
  * @param note pitch of the tone to play in Hertz (Hz)
  */
     //% blockId=music_play_note
-    //% block="play tone $note"
+    //% block="play tasdf $note"
     //% note.shadow="device_note"
     //% note.defl=Note.C
     export function playasdfasdfTone(note: number) {
