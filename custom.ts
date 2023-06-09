a6aaa35697e178e1b0beab64c434d735a54a6497
OffByOne37 <129181101+OffByOne37@users.noreply.github.com>
2023-06-09 13:43:25 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ custom.ts
@@ -0,0 +1,16 @@
+
+
+namespace music {
+
+    /**
+ * Play a tone
+ * @param note pitch of the tone to play in Hertz (Hz)
+ */
+    //% blockId=music_play_note
+    //% block="play tone $note"
+    //% note.shadow="device_note"
+    //% note.defl=Note.C
+    export function playasdfasdfTone(note: number) {
+
+    }
+}
